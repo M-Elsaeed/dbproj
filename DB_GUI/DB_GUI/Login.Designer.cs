77b53e4df5b6862fc104805bbff89b77c4503204
moataz98 <36770511+moataz98@users.noreply.github.com>
2018-12-19 19:39:17 +0200

    gui

--- DB_GUI/DB_GUI/Login.Designer.cs
+++ DB_GUI/DB_GUI/Login.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.loginBtn = new System.Windows.Forms.Button();
             this.passwordField = new System.Windows.Forms.TextBox();
             this.usernameField = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -46,14 +46,14 @@
             this.label3.TabIndex = 11;
             this.label3.Text = "Welcome to ASU bank";
             // 
-            // button1
+            // loginBtn
             // 
-            this.button1.Location = new System.Drawing.Point(633, 383);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 45);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "LOGIN";
-            this.button1.UseVisualStyleBackColor = true;
+            this.loginBtn.Location = new System.Drawing.Point(633, 383);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(128, 45);
+            this.loginBtn.TabIndex = 10;
+            this.loginBtn.Text = "LOGIN";
+            this.loginBtn.UseVisualStyleBackColor = true;
             // 
             // passwordField
             // 
@@ -98,7 +98,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.loginBtn);
             this.Controls.Add(this.passwordField);
             this.Controls.Add(this.usernameField);
             this.Controls.Add(this.label2);
@@ -113,7 +113,7 @@
         #endregion
 
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button loginBtn;
         private System.Windows.Forms.TextBox passwordField;
         private System.Windows.Forms.TextBox usernameField;
         private System.Windows.Forms.Label label2;
