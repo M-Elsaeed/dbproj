df9ecdc81028a715ee0df2153f13132abf63b54a
M-Elsaeed <m.elsaeed1998@gmail.com>
2018-12-28 19:26:52 +0200

    Update CityAvg.Designer.cs

--- DB_GUI/DB_GUI/CityAvg.Designer.cs
+++ DB_GUI/DB_GUI/CityAvg.Designer.cs
@@ -100,6 +100,7 @@
             this.Controls.Add(this.cityText);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "CityAvg";
             this.Text = "Find Average Balance By City";
             this.ResumeLayout(false);
